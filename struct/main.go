70139ca22ac2e89c2c2ae8fec8cd38c9172eef27
awisu2 <awisu2@gmail.com>
2021-09-29 08:56:22 +0900

    ストラクトの内部宣言時サンプル追加

--- struct/main.go
+++ struct/main.go
@@ -10,8 +10,15 @@ type Sample struct {
 	B string `json:"b"`
 }
 
+// Sampleを直接宣言し、同じフィールドを設定した場合
+type Sample2 struct {
+	Sample
+	B string `json:"b"`
+}
+
 func main() {
 	tagInfo()
+	override()
 }
 
 func tagInfo () {
@@ -24,4 +31,28 @@ func tagInfo () {
 	}
 
 	fmt.Println(field.Tag) // json:"a", foo:"bar"
+}
+
+func override() {
+	s2 := Sample2 {
+		// セット時、直接のアクセスはできない
+		// A: "a",
+		Sample: Sample{
+			A: "a",
+			B: "b1",
+		},
+		B: "b2",
+	}
+
+	fmt.Println(s2) // {{a b1} b2}
+
+	// 宣言後は直接のアクセスが可能
+	s2.A = "aaa"
+	s2.Sample.B = "bbb1"
+	s2.B = "bbb2"
+
+	// 取得時も同じく直接アクセス可能
+	fmt.Println(s2.A) // aaa
+	// 元になったstructのプロパティが優先される
+	fmt.Println(s2.B) // bbb2
 }
