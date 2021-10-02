4f04602cd6d75fe11a7f96dc2ed02da4631d89f7
awisu2 <awisu2@gmail.com>
2021-10-03 08:17:45 +0900

    エラーで通らないが一応nodeのサンプル

--- sharedGo/README.md
+++ sharedGo/README.md
@@ -2,6 +2,11 @@
 
 go を.so ファイルにビルドし、他言語に組み込む勉強
 
+1. 一定の要件を満たしたコードを記述
+2. .so ファイルのビルド
+   - `go build -o awesome.so -buildmode=c-shared .`
+3. 各種言語から呼び出し
+
 ## links
 
 - [Calling Go Functions from Other Languages | by Vladimir Vivien | Learning the Go Programming Language | Medium](https://medium.com/learning-the-go-programming-language/calling-go-functions-from-other-languages-4c7d8bcc69bf)
---
+++ sharedGo/fromnode/client.js
@@ -0,0 +1,41 @@
+var ref = require('ref-napi')
+var ffi = require('ffi-napi')
+var Struct = require('ref-struct-napi')
+var ArrayType = require('ref-array-napi')
+var LongArray = ArrayType(ref.types.longlong)
+
+var GoSlice = Struct({
+  data: LongArray,
+  len: 'longlong',
+  cap: 'longlong'
+})
+var GoString = Struct({
+  p: 'string',
+  n: 'longlong'
+})
+
+var file = './awesome.so'
+var fs = require('fs')
+if (!fs.existsSync(file)) {
+  console.log('not exists file. ' + file)
+}
+
+// TOOD: Dynamic Linking Error: Win32 error 126 と出て通らない
+var awesome = ffi.Library(file, {
+  Add: ['longlong', ['longlong', 'longlong']],
+  Cosine: ['double', ['double']],
+  Sort: ['void', [GoSlice]],
+  Log: ['longlong', [GoString]]
+})
+console.log('awesome.Add(12, 99) = ', awesome.Add(12, 99))
+console.log('awesome.Cosine(1) = ', awesome.Cosine(1))
+nums = LongArray([12, 54, 0, 423, 9])
+var slice = new GoSlice()
+slice['data'] = nums
+slice['len'] = 5
+slice['cap'] = 5
+awesome.Sort(slice)
+str = new GoString()
+str['p'] = 'Hello Node!'
+str['n'] = 11
+awesome.Log(str)
